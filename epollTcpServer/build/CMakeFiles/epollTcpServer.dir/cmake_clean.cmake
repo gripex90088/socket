acee449c9e131e0807007ccd4a7f5d239a930d3d
jianronghub <1355039189@qq.com>
2019-09-12 10:35:09 +0800

    update epollTcpServer

---
+++ epollTcpServer/build/CMakeFiles/epollTcpServer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/epollTcpServer.dir/epollTcpServer.c.o"
+  "epollTcpServer.pdb"
+  "epollTcpServer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/epollTcpServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- epollTcpServer/epollTcpServer.c
+++ epollTcpServer/epollTcpServer.c
@@ -2,7 +2,7 @@
  * epollTcpServer.c
  *
  *  Created on: Sep 6, 2019
- *      Author: xuxing
+ *      Author: JianRong
  */
 
 
@@ -10,41 +10,116 @@
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <sys/epoll.h>  // header file include epoll
+
+/****************************************************************************
+ * URL: http://www.man7.org/linux/man-pages/man7/epoll.7.html
+ * [epoll]
+ * 		epoll - I/O event notification facility
+ * header file: <sys/epoll.h>
+ *
+ * [interface]
+ * 		epoll_create(2)
+ *
+ *
+ ***************************************************************************/
 
 int main()
 {
+	int i, rest, _sSock, _cSock, _sockLen, epfd, fd_num, time_out, read_len;
 
-	int fd, con_fd, _sockLen;
-	struct sockaddr_in serv_sock;
+	struct sockaddr_in serv_addr;
+	struct sockaddr_in client_addr;
+	struct epoll_event ep_event;
+	struct epoll_event ep_events[10];
+	char buf[BUFSIZ];
 
-	if ((fd = socket(PF_INET, SOCK_STREAM, 0)) == -1) {
+	if ((_sSock = socket(PF_INET, SOCK_STREAM, 0)) == -1) {
 		printf("establish socket failed\n");
 		exit(EXIT_FAILURE);
 	}
-	printf("establish socket success, <socket=%d>\n", serv_sock);
+	printf("establish socket success, <socket=%d>\n", _sSock);
 
-	serv_sock.sin_family = AF_INET;
-	serv_sock.sin_port = htons(8081);
-	serv_sock.sin_addr.s_addr = INADDR_ANY;
-	bzero(&(serv_sock.sin_zero), 8);
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(20201);
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	bzero(&(serv_addr.sin_zero), 8);
 	_sockLen = sizeof(struct sockaddr_in);
 
-	if ( -1 == bind(fd, (struct sockaddr *)&serv_sock, _sockLen) ) {
+	if ( -1 == bind(_sSock, (struct sockaddr *)&serv_addr, _sockLen) ) {
 		printf("bind failed\n");
 		exit(EXIT_FAILURE);
 	}
 	printf("bind success\n");
 
-	if ( -1 == listen(fd, 0)) {
+	if ( -1 == listen(_sSock, 0)) {
 		printf("listen failed\n");
 		exit(EXIT_FAILURE);
 	}
 	printf("listen success\n");
 
-	while(1) {
+	epfd = epoll_create(2);
 
+	ep_event.events = EPOLLIN;
+	ep_event.data.fd = _sSock;
+	if ( -1 == epoll_ctl(epfd, EPOLL_CTL_ADD, _sSock, &ep_event) ) {
+		printf("epoll create failure\n");
+		exit(EXIT_FAILURE);
 	}
 
-	close(fd);
+	fd_num = 0;
+	time_out = -1;
+	while(1) {
+		fd_num = epoll_wait(epfd, ep_events, 10, time_out);
+
+		for (i = 0; i < fd_num; i++) {
+
+			_cSock = accept(_sSock, (struct sockaddr*)&client_addr, (socklen_t *)&_sockLen);
+			// new client join
+			if (_sSock == ep_events[i].data.fd) {
+				if (-1 == _cSock) {
+					printf("accept failure\n");
+					continue;
+				} else {
+					ep_event.events = EPOLLIN;
+					ep_event.data.fd = _cSock;
+					epoll_ctl(epfd, EPOLL_CTL_ADD, _cSock, &ep_event);
+				}
+				printf("New client join, <socket=%d>\n", _cSock);
+
+				// recv data
+			} else if (ep_events[i].events & EPOLLIN) {
+
+				_cSock = ep_events[i].data.fd;
+				read_len = read(_cSock, buf, BUFSIZ);
+				printf("%d\n", buf[0]);
+				if (read_len <= 0) {
+					printf("<socket=%d> exit\n", _cSock);
+					// client quit
+					epoll_ctl(epfd, EPOLL_CTL_DEL, _cSock, NULL);
+					close(_cSock);
+					ep_events[i].data.fd = -1;
+					continue;
+				}
+
+				ep_event.data.fd = _cSock;
+				ep_event.events = EPOLLOUT;
+				epoll_ctl(epfd, EPOLL_CTL_MOD, _cSock, &ep_event);
+
+				// send msg
+			} else if (ep_events[i].events & EPOLLOUT) {
+				_cSock = ep_events[i].data.fd;
+				rest = send(_cSock, buf, BUFSIZ, 0);
+
+				ep_event.events = EPOLLIN;
+				ep_event.events = _cSock;
+				epoll_ctl(epfd, EPOLL_CTL_MOD, _cSock, &ep_event);
+			}
+
+		}
+	}
+
+	close(epfd);
+	close(_sSock);
 	exit(EXIT_SUCCESS);
 }
